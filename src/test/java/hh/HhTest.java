369d7f999436dcfdde22e8d0b69bc47997562929
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-09-05 17:38:43 +0300

    Update HhTest.java

--- src/test/java/hh/HhTest.java
+++ src/test/java/hh/HhTest.java
@@ -13,7 +13,7 @@ public class HhTest extends BaseTest {
     private final static String URL = "https://hh.ru/applicant/resumes/view?resume=1edf0c93ff095811d20039ed1f6a3638497073";
 
     @Test
-    public void checkAttributesHashMap(){
+    public void checkAttributesHashMap() {
         HhResumePage hhResumePage = new HhResumePage(URL);
         Map<String,Object> expectedAttributes = new HashMap<>();
         expectedAttributes.put(HhResumePage.GENDER, "М");
@@ -28,7 +28,7 @@ public class HhTest extends BaseTest {
     }
 
     @Test
-    public void checkAttributesClass(){
+    public void checkAttributesClass() {
         HhResumePage hhResumePage = new HhResumePage(URL);
         Resume expectedAttributes = new Resume("М", 26,"Санкт-Петербург",
                 true,false);
@@ -36,16 +36,16 @@ public class HhTest extends BaseTest {
                 hhResumePage.isPhoneConfirmed(),hhResumePage.isReadyToRelocate());
 
         /** 
-            1 способ сравнения классов
-         Assert.assertTrue(EqualsBuilder.reflectionEquals(expectedAttributes,actualAttributes));
-        */
+         *   1 способ сравнения классов
+         * Assert.assertTrue(EqualsBuilder.reflectionEquals(expectedAttributes,actualAttributes));
+         */
+        
         // 2 способ сравнения отдельных переменных в классе
+       
         assertEquals(expectedAttributes.getGender(),actualAttributes.getGender());
         assertEquals(expectedAttributes.getAge(),actualAttributes.getAge());
         assertEquals(expectedAttributes.getCity(),actualAttributes.getCity());
         assertEquals(expectedAttributes.isPhoneConfirmed(),actualAttributes.isPhoneConfirmed());
         assertEquals(expectedAttributes.isReadyToRelocate(),actualAttributes.isReadyToRelocate());
-
-
     }
 }
